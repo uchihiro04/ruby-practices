75cc69584a6fdc8ee733b8266d3e38085f8dab42
uchihiro <tohchi4122@gmail.com>
2022-09-27 17:21:20 +0900

    filenameの取得場所を変更
    ファイル名・ディレクトリ名を取得する場所をCommandクラスからLsFormatFactoryモジュールに変更しました。

--- 08.ls_object/lib/command.rb
+++ 08.ls_object/lib/command.rb
@@ -4,30 +4,14 @@ require_relative 'ls_format_factory'
 
 module Ls
   class Command
-    def initialize(option = nil)
+    def initialize(option)
       @option = option
     end
 
     def run_ls
-      ls_format = LsFormatFactory.create(filenames, l_option?)
+      ls_format = LsFormatFactory.create(@option)
       ls_format.output_format
     end
 
-    def filenames
-      collected_filenames = a_option? ? Dir.glob('*', ::File::FNM_DOTMATCH) : Dir.glob('*')
-      r_option? ? collected_filenames.reverse : collected_filenames
-    end
-
-    def a_option?
-      @option['a'] == true
-    end
-
-    def r_option?
-      @option['r'] == true
-    end
-
-    def l_option?
-      @option['l'] == true
-    end
   end
 end
--- 08.ls_object/lib/ls_format_factory.rb
+++ 08.ls_object/lib/ls_format_factory.rb
@@ -2,7 +2,9 @@ require_relative 'normal_format'
 require_relative 'long_format'
 
 module LsFormatFactory
-  def self.create(filenames, l_option)
-    l_option ? Ls::LongFormat.new(filenames) : Ls::NormalFormat.new(filenames)
+  def self.create(option)
+    filenames = option['a'] ? Dir.glob('*', ::File::FNM_DOTMATCH) : Dir.glob('*')
+    filenames = option['r'] ? filenames.reverse : filenames
+    option['l'] ? Ls::LongFormat.new(filenames) : Ls::NormalFormat.new(filenames)
   end
 end
